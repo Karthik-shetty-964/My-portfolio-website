d49a5655a8da7ce9f4018e0e96b00bccb3e56b5f
Karthik shetty <karthiksshetty111@gmail.com>
2022-01-16 18:13:18 +0530

    project completed except project preloader

--- js/main.js
+++ js/main.js
@@ -1,4 +1,81 @@
 
+
+/*----------navigation menu----------*/
+(()=>{
+    const hamburgerBtn=document.querySelector(".hamburger-btn");
+    navMenu=document.querySelector(".nav-menu");
+    closeNavBtn=navMenu.querySelector(".close-nav-menu");
+
+    hamburgerBtn.addEventListener("click",showNavMenu);
+    closeNavBtn.addEventListener("click",hideNavMenu);
+
+    function showNavMenu(){
+        navMenu.classList.add("open");
+        bodyScrollingToggle();
+    }
+    function hideNavMenu(){
+        navMenu.classList.remove("open");
+        fadeOutEffect();
+        bodyScrollingToggle();
+    }
+    function fadeOutEffect(){
+        document.querySelector(".fade-out-effect").classList.add("active");
+        setTimeout(()=>{
+            document.querySelector(".fade-out-effect").classList.remove("active");
+        },300)
+    }
+
+    //attach an event handler to document
+    document.addEventListener("click",(event)=>{
+        if(event.target.classList.contains("link-item")){
+            /*making sure event.target.hash has a value before overriding default behaviour */
+            if(event.target.hash !== ""){
+                //prevent default anchor click behavior
+                event.preventDefault();
+                const hash=event.target.hash;
+                console.log(hash);
+                //deactivate existing active class and new active class
+                document.querySelector(".section.active").classList.add("hide");
+                document.querySelector(".section.active").classList.remove("active");
+
+                document.querySelector(hash).classList.add("active");
+                document.querySelector(hash).classList.remove("hide");
+
+                //deactivate existing active navigation menu 
+                navMenu.querySelector(".active").classList.add("outer-shadow","hover-in-shadow");
+                navMenu.querySelector(".active").classList.remove("active","inner-shadow");
+                /*if clicked 'link-item is contained withing the navigation menu' */
+                if(navMenu.classList.contains("open")){
+                    //activate new navigaion menu item
+                    event.target.classList.add("active","inner-shadow");
+                    event.target.classList.remove("outer-shadow","hover-in-shadow");
+                    //hide navigation menu item
+                    hideNavMenu();
+                }
+                else{
+                    let navItems =navMenu.querySelectorAll(".link-item");
+                    navItems.forEach((item) =>{
+                        if(hash === item.hash){
+                            //activate new navigation menu link-item
+                            item.classList.add("active","inner-shadow");
+                            item.classList.remove("outer-shadow","hover-in-shadow");
+                        }
+                    }) 
+                    fadeOutEffect();               
+                }
+                //add hash (#) to url
+                window.location.hash = hash;
+            }
+        }
+    })
+
+})();
+
+
+
+
+
+
 /*----------about section tabs----------*/
 
 (() =>{//this fucntion gets automatically called .
@@ -225,4 +302,19 @@ function bodyScrollingToggle(){
         sliderContainer.style.marginLeft= - (slideWidth * slideIndex)+"px";  
     }
     slider();
+})();
+
+
+
+/* --------hide all sections except active---------- */
+(()=>{
+    
+     const sections =document.querySelectorAll(".section");
+     
+     sections.forEach((section)=>{
+         if(!section.classList.contains("active")){
+             section.classList.add("hide");
+         }
+     })
+
 })();
