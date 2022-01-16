5aeec107fa3dd122b9fb729c32d70ab7db3e835d
Karthik shetty <karthiksshetty111@gmail.com>
2022-01-16 22:03:35 +0530

    Completed front end

--- js/main.js
+++ js/main.js
@@ -318,3 +318,13 @@ function bodyScrollingToggle(){
      })
 
 })();
+
+
+/*------project preloader------ */
+window.addEventListener("load",()=>{
+    //preloader
+    document.querySelector(".preloader").classList.add("fade-out");
+    setTimeout(()=>{
+        document.querySelector(".preloader").style.display="none";
+    },600);
+})
